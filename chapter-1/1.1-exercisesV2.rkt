01100547b8b2cdae3e0493cb2ed2d181cf87302d
Victor Forbes <vhforbes@gmail.com>
2024-04-08 09:46:07 -0300

    feat: ex 1.3 after a logic battle

--- chapter-1/1.1-exercisesV2.rkt
+++ chapter-1/1.1-exercisesV2.rkt
@@ -29,14 +29,37 @@
 ;1.3
 (define (square a) (* a a))
 
+(define (sum_of_squares a b) (+ (square a) (square b)))
+
 (define (largest a b c)
-  (cond ((and (> a b) (> a c)) a)
-        ((and (> b a) (> b c)) b)
-        ((and (> c a) (> c b)) c)
-        )
+  (cond
+    ((and (> a b) (> a c)) a)
+    ((and (> b a) (> b c)) b)
+    ((and (> c a) (> c b)) c)
+    )
   )
 
-(largest a b c)
+(largest 5 7 3)
+
+(define (second_largest a b c)
+  (cond
+    ((or (and (> a b) (> a c)) (and (> a c) (< a b))) a)
+    ((or (and (> b a) (> c b)) (and (> b c) (< b a))) b)
+    ((or (and (> c a) (> b c)) (and (> c b) (< c a))) c)
+    )
+  )
+
+(second_largest 5 7 3)
+
+
+
+(define (sum_square_two_largests a b c)
+  (+ (sum_of_squares (largest a b c) (second_largest a b c)))
+  )
+
+(sum_square_two_largests 5 7 3)
+
+
 
 
 
